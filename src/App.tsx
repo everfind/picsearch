29ec51d2ec538df4494a8342d47dd44d89e2e2b0
bingoootang <bingooo_tang@163.com>
2021-11-11 10:49:49 +0800

    fix: 滚动样式

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
   const { q: keyword } = qs.parse(window.location.search.substring(1));
 
   return (
-    <div className={cls('w-screen', 'h-screen', 'bg-gray-100')}>
+    <div className={cls('w-screen', 'h-screen', 'bg-gray-100', 'overflow-x-hidden')}>
       <div className={cls('flex', 'flex-row', 'justify-center', 'p-4')}>
         <Search query={keyword as string} />
       </div>
