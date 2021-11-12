29a90e4e1c98fb993ab5bb3e010bf6ee39801939
bingoootang <bingooo_tang@163.com>
2021-11-12 19:07:55 +0800

    feat: add placeholder for picture list

--- src/components/picture-list/index.tsx
+++ src/components/picture-list/index.tsx
@@ -3,32 +3,49 @@ import cls from 'clsx';
 import { Picture } from '../../types';
 
 export function PictureList({ pictureList }: { pictureList: Picture[] }) {
-  return (
-    <ul className={cls('flex', 'flex-row', 'flex-wrap')}>
-      {pictureList.map((pic) => {
-        const rate = pic.width / pic.height;
-        const baseHeight = 250;
-        return (
-          <a
-            key={pic.id}
-            className={cls('relative', 'm-1', 'block')}
-            style={{
-              width: `${rate * baseHeight}px`,
-              flexGrow: rate * baseHeight,
-              flexShrink: rate * baseHeight,
-              background: '#f6f5fa',
-              maxHeight: '50vh',
-              maxWidth: `calc(${rate} * 50vh)`,
-            }}
-            href={pic.pageUrl}
-            target='_blank'
-            rel='noreferrer'
-          >
-            <i className={cls('block')} style={{ paddingBottom: `${(1 / rate) * 100}%` }}></i>
-            <img className={cls('absolute', 'w-full', 'h-full', 'top-0', 'align-bottom', 'border', 'lazyload')} data-src={pic.imageUrl} alt='' />
-          </a>
-        );
-      })}
-    </ul>
-  );
+  let list;
+  if (pictureList.length) {
+    list = pictureList.map((pic) => {
+      const rate = pic.width / pic.height;
+      const baseHeight = 250;
+      return (
+        <a
+          key={pic.id}
+          className={cls('relative', 'm-1', 'block')}
+          style={{
+            width: `${rate * baseHeight}px`,
+            flexGrow: rate * baseHeight,
+            flexShrink: rate * baseHeight,
+            background: '#f6f5fa',
+            maxHeight: '50vh',
+            maxWidth: `calc(${rate} * 50vh)`,
+          }}
+          href={pic.pageUrl}
+          target='_blank'
+          rel='noreferrer'
+        >
+          <i className={cls('block')} style={{ paddingBottom: `${(1 / rate) * 100}%` }}></i>
+          <img className={cls('absolute', 'w-full', 'h-full', 'top-0', 'align-bottom', 'border', 'lazyload')} data-src={pic.imageUrl} alt='' />
+        </a>
+      );
+    });
+  } else {
+    list = new Array(40).fill(0).map((t, index) => (
+      <div
+        key={index}
+        className={cls('relative', 'm-1', 'block')}
+        style={{
+          width: `250px`,
+          flexGrow: 250,
+          flexShrink: 250,
+          background: '#f6f5fa',
+          maxHeight: '50vh',
+          maxWidth: '50vh',
+        }}
+      >
+        <i className={cls('block')} style={{ paddingBottom: `100%` }}></i>
+      </div>
+    ));
+  }
+  return <ul className={cls('flex', 'flex-row', 'flex-wrap')}>{list}</ul>;
 }
--- src/pages/curated/index.tsx
+++ src/pages/curated/index.tsx
@@ -1,10 +1,11 @@
 import { useDefaultPictures } from '../../lib/picture';
 import { PictureList } from '../../components/picture-list';
+import cls from 'clsx';
 
 export function CuratedPictures({ className }: { className?: string }) {
   const defaultPictures = useDefaultPictures();
   return (
-    <div className={className}>
+    <div className={cls('pt-2', className)}>
       <PictureList pictureList={defaultPictures} />
     </div>
   );
